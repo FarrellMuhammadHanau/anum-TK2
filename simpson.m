25689490691f82629b87230fb6217c0ef5d1b59e
farrell MH <hanau.muhammad@gmail.com>
2024-05-27 19:26:23 +0700

    fix simpson

--- simpson.m
+++ simpson.m
@@ -2,12 +2,12 @@ function retval = simpson(f, a, b, n)
     dif = (b - a) / (n-1);
 
     ganjil = 0;
-    for i = 1:(floor(n/2))
-        ganjil = ganjil + f(a + dif * 2 * i - 1);
+    for i = 1:(n/2)
+        ganjil = ganjil + f(a + dif * (2 * i - 1));
     endfor
 
     genap = 0;
-    for i = 1:(floor(n/2) - 1)
+    for i = 1:(n/2 - 1)
         genap = genap + f(a + dif * 2 * i);
     endfor
     
